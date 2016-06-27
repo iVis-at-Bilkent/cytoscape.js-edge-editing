20ab6dd785b7d8fe7d8f5ad23d7f878c8c73480a
metincansiper <metincansiper@gmail.com>
2016-06-27 17:13:29 +0300

    Working on UI utilities

---
+++ src/UIUtilities.js
@@ -0,0 +1,247 @@
+var debounce = require('./debounce');
+var bendPointUtilities = require('./bendPointUtilities');
+
+module.exports = function (params) {
+  var fn = params;
+
+  var ePosition, eRemove, eZoom, eSelect, eUnselect;
+  var functions = {
+    init: function () {
+      var self = this;
+      var opts = params;
+      var $container = $(this);
+      var cy;
+      var $canvas = $('<canvas></canvas>');
+
+      $container.append($canvas);
+
+      var _sizeCanvas = debounce(function () {
+        $canvas
+          .attr('height', $container.height())
+          .attr('width', $container.width())
+          .css({
+            'position': 'absolute',
+            'top': 0,
+            'left': 0,
+            'z-index': '999'
+          })
+        ;
+
+        setTimeout(function () {
+          var canvasBb = $canvas.offset();
+          var containerBb = $container.offset();
+
+          $canvas
+            .css({
+              'top': -(canvasBb.top - containerBb.top),
+              'left': -(canvasBb.left - containerBb.left)
+            })
+          ;
+
+          // redraw on canvas resize
+          if(cy){
+            clearDraws(true);
+          }
+        }, 0);
+
+      }, 250);
+
+      function sizeCanvas() {
+        _sizeCanvas();
+      }
+
+      sizeCanvas();
+
+      $(window).bind('resize', function () {
+        sizeCanvas();
+      });
+
+      var ctx = $canvas[0].getContext('2d');
+
+      // write options to data
+      var data = $container.data('cyedgebendediting');
+      if (data == null) {
+        data = {};
+      }
+      data.options = opts;
+
+      var optCache;
+
+      function options() {
+        return optCache || (optCache = $container.data('cyedgebendediting').options);
+      }
+
+      function clearDraws(renderSelectedBendShapes) {
+
+        var w = $container.width();
+        var h = $container.height();
+
+        ctx.clearRect(0, 0, w, h);
+        
+        if( renderSelectedBendShapes ) {
+          var selectedEdges = cy.edges(':selected');
+        
+          for( var i = 0; i < selectedEdges.length; i++ ) {
+            var edge = selectedEdges[i];
+            renderBendShapes(edge);
+          }
+        }
+      }
+      
+      // render the bend shapes of the given edge
+      function renderBendShapes(edge) {
+        var cy = edge.cy();
+        
+        var segpts = edge._private.rscratch.segpts;
+        var length = getBendShapesLenght(edge);
+
+        for(var i = 0; segpts && i < segpts.length; i = i + 2){
+          var bendX = segpts[i];
+          var bendY = segpts[i + 1];
+
+          var oldStyle = ctx.fillStyle;
+          ctx.fillStyle = edge.css('line-color');
+          renderBendShape(bendX, bendY, length);
+          ctx.fillStyle = oldStyle;
+        }
+      }
+      
+      // render a bend shape with the given parameters
+      function renderBendShape(bendX, bendY, length) {
+        // get the top left coordinates
+        var topLeftX = bendX - length / 2;
+        var topLeftY = bendY - length / 2;
+        
+        // render bend shape
+        ctx.rect(topLeftX, topLeftY, length, length);
+        ctx.fill();
+      }
+      
+      // get the length of bend points to be rendered
+      function getBendShapesLenght(edge) {
+        var factor = 6;
+        var length = parseFloat(edge.css('width')) * factor;
+        return length;
+      }
+      
+      // check if the point represented by {x, y} is inside the bend shape
+      function checkIfInsideBendShape(x, y, length, centerX, centerY){
+        var minX = centerX - length / 2;
+        var maxX = centerX + length / 2;
+        var minY = centerY - length / 2;
+        var maxY = centerY + length / 2;
+        
+        var inside = (x >= minX && x <= maxX) && (y >= minY && y <= maxY);
+        return inside;
+      }
+
+      // get tge index of bend point containing the point represented by {x, y}
+      function getContainingBendShapeIndex(x, y, edge) {
+        if(edge.data('weights') == null || edge.data('weights').lenght == 0){
+          return -1;
+        }
+
+        var segpts = edge._private.rscratch.segpts;
+        var length = getBendShapesLenght(edge);
+
+        for(var i = 0; segpts && i < segpts.length; i = i + 2){
+          var bendX = segpts[i];
+          var bendY = segpts[i + 1];
+
+          var inside = checkIfInsideBendShape(x, y, length, bendX, bendY);
+          if(inside){
+            return i / 2;
+          }
+        }
+
+        return -1;
+      };
+
+      // last status of gestures
+      var lastPanningEnabled, lastZoomingEnabled;
+      
+      // store the current status of gestures and set them to false
+      function disableGestures() {
+        lastPanningEnabled = cy.panningEnabled();
+        lastZoomingEnabled = cy.zoomingEnabled();
+
+        cy.zoomingEnabled(false)
+          .panningEnabled(false);
+      }
+      
+      // reset the gestures by their latest status
+      function resetGestures() {
+        cy.zoomingEnabled(lastZoomingEnabled)
+          .panningEnabled(lastPanningEnabled);
+      }
+
+      $container.cytoscape(function (e) {
+        cy = this;
+        clearDraws(true);
+        
+        lastPanningEnabled = cy.panningEnabled();
+        lastZoomingEnabled = cy.zoomingEnabled();
+        
+        // define edgebendediting-hasbendpoints css class
+        cy.style().selector('.edgebendediting-hasbendpoints').css({
+          'curve-style': 'segments',
+          'segment-distances': function (ele) {
+            return bendPointUtilities.getSegmentDistancesString(ele);
+          },
+          'segment-weights': function (ele) {
+            return bendPointUtilities.getSegmentWeightsString(ele);
+          },
+          'edge-distances': 'node-position'
+        });
+        
+        cy.bind('zoom pan', eZoom = function () {
+          clearDraws(true);
+        });
+
+        cy.on('position', 'node', ePosition = function () {
+          var node = this;
+          
+          // TODO
+        });
+
+        cy.on('remove', 'edge', eRemove = function () {
+          var node = this;
+          
+          // TODO
+        });
+        
+        cy.on('select', 'edge', eSelect = function () {
+          var edge = this;
+          
+          // TODO
+        });
+        
+        cy.on('unselect', 'edge', eUnselect = function () {
+          var edge = this;
+          
+          // TODO
+        });
+      });
+
+      $container.data('cyedgebendediting', data);
+    },
+    unbind: function () {
+        cy.off('position', 'node', ePosition)
+          .off('remove', 'node', eRemove)
+          .off('select', 'edge', eSelect)
+          .off('unselect', 'edge', eUnselect);
+
+        cy.unbind("zoom pan", eZoom);
+    }
+  };
+
+  if (functions[fn]) {
+    return functions[fn].apply(this, Array.prototype.slice.call(arguments, 1));
+  } else if (typeof fn == 'object' || !fn) {
+    return functions.init.apply(this, arguments);
+  } else {
+    $.error('No such function `' + fn + '` for cytoscape.js-edge-bend-editing');
+  }
+
+  return $(this);
+};
