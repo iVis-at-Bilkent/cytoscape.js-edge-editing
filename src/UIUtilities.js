1623421880b43885e3d797d2e3d722b74d726556
Ahmet Çandıroğlu <ahmetcandiroglu@gmail.com>
2018-07-04 12:39:34 +0300

    Change style of bend points and reconnection handles iVis-at-Bilkent/newt#251

--- src/UIUtilities.js
+++ src/UIUtilities.js
@@ -207,7 +207,7 @@ module.exports = function (params, cy) {
           var bendY = segpts[i + 1];
 
           var oldStyle = ctx.fillStyle;
-          ctx.fillStyle = edge.css('line-color');
+          ctx.fillStyle = shadeBlend(-0.25, edge.css('line-color'));
           renderBendShape(bendX, bendY, length);
           ctx.fillStyle = oldStyle;
         }
@@ -256,8 +256,8 @@ module.exports = function (params, cy) {
         var oldWidth = ctx.lineWidth;
         var oldFill = ctx.fillStyle;
 
-        ctx.strokeStyle = edge.css('line-color');
-        ctx.lineWidth = edge.data('width') * cy.zoom() / 2;
+        ctx.strokeStyle = shadeBlend(-0.25, edge.css('line-color'));
+        ctx.lineWidth = edge.data('width') * cy.zoom();
         
         renderEachEndPointShape(src, target, length);
         
@@ -343,6 +343,19 @@ module.exports = function (params, cy) {
         }
       }
 
+      // changes color tone
+      // https://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors
+      function shadeBlend(p,c0,c1) {
+        var n=p<0?p*-1:p,u=Math.round,w=parseInt;
+        if(c0.length>7){
+          var f=c0.split(","),t=(c1?c1:p<0?"rgb(0,0,0)":"rgb(255,255,255)").split(","),R=w(f[0].slice(4)),G=w(f[1]),B=w(f[2]);
+          return "rgb("+(u((w(t[0].slice(4))-R)*n)+R)+","+(u((w(t[1])-G)*n)+G)+","+(u((w(t[2])-B)*n)+B)+")"
+        }
+        else{
+          var f=w(c0.slice(1),16),t=w((c1?c1:p<0?"#000000":"#FFFFFF").slice(1),16),R1=f>>16,G1=f>>8&0x00FF,B1=f&0x0000FF;
+          return "#"+(0x1000000+(u(((t>>16)-R1)*n)+R1)*0x10000+(u(((t>>8&0x00FF)-G1)*n)+G1)*0x100+(u(((t&0x0000FF)-B1)*n)+B1)).toString(16).slice(1)
+        }
+      }
 
       // get the length of bend points to be rendered
       function getBendShapesLength(edge) {
