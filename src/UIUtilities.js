3371f8279289aa9026eee1582f683615986d7f65
Nasim Saleh <nasimyhsaleh@gmail.com>
2019-09-12 10:16:14 +0300

    fixes #24

--- src/UIUtilities.js
+++ src/UIUtilities.js
@@ -986,8 +986,7 @@ module.exports = function (params, cy) {
         cy.on('cyedgebendediting.changeBendPoints', 'edge', function() {
           var edge = this;
           cy.startBatch();
-          cy.edges().unselect();
-          edge.select();
+          cy.edges().unselect();          
           cy.trigger('bendPointMovement');
           cy.endBatch();
           refreshDraws();
